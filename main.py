c4c70f3baeea9ebffb2081d7c85433c9b09f2272
Ryuma Nakano <ryuma@nakasan.co>
2022-06-01 18:12:46 -0500

    Create a show_person request to search for a person with required query parameters

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # Python
+from optparse import Option
 from typing import Optional
 
 # Pydantic
 from pydantic import BaseModel
 
 # FastAPI
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Query
 
 
 app = FastAPI()
@@ -24,5 +25,16 @@ def home():
 
 # Request and response body
 @app.post("/person/new")
-def create_person(person: Person = Body(...)):
+def create_person(
+    person: Person = Body(...) # required body parameter
+):
     return person
+
+# Validations: Query parameters
+
+@app.get("/person/detail")
+def show_person(
+    name: Optional[str] = Query(default=None, min_length=1, max_length=50),
+    age: str = Query(...) # required query parameter
+):
+    return {name: age}
