9a21d8035a11da06985892b3f84dba572cea29cf
Ryuma Nakano <rjnl_85@hotmail.com>
2022-06-06 17:07:57 -0400

    Create a path parameter function and analyze the differences with the query parameters

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 # Python
-from optparse import Option
 from typing import Optional
 
 # Pydantic
 from pydantic import BaseModel
 
 # FastAPI
-from fastapi import FastAPI, Body, Query
+from fastapi import FastAPI, Body, Path, Query
 
 
 app = FastAPI()
@@ -34,7 +33,28 @@ def create_person(
 
 @app.get("/person/detail")
 def show_person(
-    name: Optional[str] = Query(default=None, min_length=1, max_length=50),
-    age: int = Query(default=...,ge=18) # required query parameter and greater or equal than 18
+    name: Optional[str] = Query(
+        default=None, 
+        min_length=1, 
+        max_length=50,
+        title="Person Name",
+        description="This is the person's fullname. It has to be between 1 and 50 characters"
+        ),
+    age: int = Query(
+        default=...,
+        ge=18,
+        title="Person Age",
+        description="This is the person's age. It's required and has to be greater on equal than 18"
+        ) # required query parameter and greater or equal than 18 (ge,gt,le,lt)
 ):
     return {name: age}
+
+# Validations: Path Parameters
+@app.get("/person/detail/{person_id}")
+def show_person(
+    person_id: int = Path(
+        default=...,
+        gt=0
+        )
+):
+    return {person_id: f"It exists!"} 
